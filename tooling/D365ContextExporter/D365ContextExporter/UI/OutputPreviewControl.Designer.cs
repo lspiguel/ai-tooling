a0be57b76b748d7025ca337a422b3e4db792a7b7
Luciano Spiguel <46658141+lspiguel@users.noreply.github.com>
2026-05-09 00:18:25 -0300

    UI Fixes.

--- tooling/D365ContextExporter/D365ContextExporter/UI/OutputPreviewControl.Designer.cs
+++ tooling/D365ContextExporter/D365ContextExporter/UI/OutputPreviewControl.Designer.cs
@@ -27,94 +27,116 @@
             this.mainLayout.SuspendLayout();
             this.projectButtonPanel.SuspendLayout();
             this.SuspendLayout();
-
+            // 
             // mainLayout
+            // 
             this.mainLayout.ColumnCount = 2;
-            this.mainLayout.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.AutoSize));
+            this.mainLayout.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
             this.mainLayout.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
             this.mainLayout.Controls.Add(this.lblBanner, 0, 0);
             this.mainLayout.Controls.Add(this.lblProjectOutput, 0, 1);
             this.mainLayout.Controls.Add(this.txtProjectOutput, 1, 1);
             this.mainLayout.Controls.Add(this.projectButtonPanel, 0, 2);
             this.mainLayout.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.mainLayout.Location = new System.Drawing.Point(0, 0);
             this.mainLayout.Name = "mainLayout";
             this.mainLayout.RowCount = 3;
-            this.mainLayout.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.AutoSize));
-            this.mainLayout.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.AutoSize));
-            this.mainLayout.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.AutoSize));
-            this.mainLayout.Size = new System.Drawing.Size(784, 80);
+            this.mainLayout.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.mainLayout.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.mainLayout.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.mainLayout.Size = new System.Drawing.Size(672, 88);
             this.mainLayout.TabIndex = 0;
-
+            // 
             // lblBanner
-            this.mainLayout.SetColumnSpan(this.lblBanner, 2);
+            // 
             this.lblBanner.AutoSize = true;
-            this.lblBanner.BackColor = System.Drawing.Color.FromArgb(232, 244, 255);
+            this.lblBanner.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(232)))), ((int)(((byte)(244)))), ((int)(((byte)(255)))));
+            this.mainLayout.SetColumnSpan(this.lblBanner, 2);
             this.lblBanner.Dock = System.Windows.Forms.DockStyle.Fill;
             this.lblBanner.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold);
-            this.lblBanner.ForeColor = System.Drawing.Color.FromArgb(0, 84, 166);
+            this.lblBanner.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(84)))), ((int)(((byte)(166)))));
+            this.lblBanner.Location = new System.Drawing.Point(3, 0);
             this.lblBanner.Name = "lblBanner";
-            this.lblBanner.Padding = new System.Windows.Forms.Padding(6, 4, 6, 4);
+            this.lblBanner.Padding = new System.Windows.Forms.Padding(5, 3, 5, 3);
+            this.lblBanner.Size = new System.Drawing.Size(666, 21);
             this.lblBanner.TabIndex = 0;
             this.lblBanner.Text = "Export complete. Upload the context file to your AI assistant.";
-
+            // 
             // lblProjectOutput
+            // 
             this.lblProjectOutput.AutoSize = true;
+            this.lblProjectOutput.Location = new System.Drawing.Point(3, 21);
             this.lblProjectOutput.Name = "lblProjectOutput";
-            this.lblProjectOutput.Padding = new System.Windows.Forms.Padding(0, 5, 6, 0);
+            this.lblProjectOutput.Padding = new System.Windows.Forms.Padding(0, 4, 5, 0);
+            this.lblProjectOutput.Size = new System.Drawing.Size(67, 17);
             this.lblProjectOutput.TabIndex = 1;
             this.lblProjectOutput.Text = "Context file:";
-
+            // 
             // txtProjectOutput
+            // 
             this.txtProjectOutput.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.txtProjectOutput.Location = new System.Drawing.Point(76, 24);
             this.txtProjectOutput.Name = "txtProjectOutput";
             this.txtProjectOutput.ReadOnly = true;
+            this.txtProjectOutput.Size = new System.Drawing.Size(593, 20);
             this.txtProjectOutput.TabIndex = 2;
-
+            // 
             // projectButtonPanel
-            this.mainLayout.SetColumnSpan(this.projectButtonPanel, 2);
+            // 
             this.projectButtonPanel.AutoSize = true;
+            this.mainLayout.SetColumnSpan(this.projectButtonPanel, 2);
             this.projectButtonPanel.Controls.Add(this.btnOpenProject);
             this.projectButtonPanel.Controls.Add(this.btnRevealProject);
             this.projectButtonPanel.Controls.Add(this.btnCopyProject);
             this.projectButtonPanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.projectButtonPanel.Location = new System.Drawing.Point(3, 50);
             this.projectButtonPanel.Name = "projectButtonPanel";
             this.projectButtonPanel.Padding = new System.Windows.Forms.Padding(0, 2, 0, 2);
+            this.projectButtonPanel.Size = new System.Drawing.Size(666, 35);
             this.projectButtonPanel.TabIndex = 3;
-
+            // 
             // btnOpenProject
+            // 
+            this.btnOpenProject.Location = new System.Drawing.Point(3, 5);
             this.btnOpenProject.Name = "btnOpenProject";
-            this.btnOpenProject.Size = new System.Drawing.Size(60, 23);
+            this.btnOpenProject.Size = new System.Drawing.Size(51, 20);
             this.btnOpenProject.TabIndex = 0;
             this.btnOpenProject.Text = "Open";
             this.btnOpenProject.UseVisualStyleBackColor = true;
             this.btnOpenProject.Click += new System.EventHandler(this.btnOpenProject_Click);
-
+            // 
             // btnRevealProject
+            // 
+            this.btnRevealProject.Location = new System.Drawing.Point(60, 5);
             this.btnRevealProject.Name = "btnRevealProject";
-            this.btnRevealProject.Size = new System.Drawing.Size(60, 23);
+            this.btnRevealProject.Size = new System.Drawing.Size(51, 20);
             this.btnRevealProject.TabIndex = 1;
             this.btnRevealProject.Text = "Reveal";
             this.btnRevealProject.UseVisualStyleBackColor = true;
             this.btnRevealProject.Click += new System.EventHandler(this.btnRevealProject_Click);
-
+            // 
             // btnCopyProject
+            // 
+            this.btnCopyProject.Location = new System.Drawing.Point(117, 5);
             this.btnCopyProject.Name = "btnCopyProject";
-            this.btnCopyProject.Size = new System.Drawing.Size(75, 23);
+            this.btnCopyProject.Size = new System.Drawing.Size(64, 20);
             this.btnCopyProject.TabIndex = 2;
             this.btnCopyProject.Text = "Copy Path";
             this.btnCopyProject.UseVisualStyleBackColor = true;
             this.btnCopyProject.Click += new System.EventHandler(this.btnCopyProject_Click);
-
+            // 
             // OutputPreviewControl
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.Controls.Add(this.mainLayout);
             this.Name = "OutputPreviewControl";
-            this.Size = new System.Drawing.Size(784, 80);
+            this.Size = new System.Drawing.Size(672, 88);
             this.mainLayout.ResumeLayout(false);
             this.mainLayout.PerformLayout();
             this.projectButtonPanel.ResumeLayout(false);
             this.ResumeLayout(false);
+
         }
 
         private System.Windows.Forms.TableLayoutPanel mainLayout;
--- tooling/D365ContextExporter/D365ContextExporter/UI/SpecPickerControl.Designer.cs
+++ tooling/D365ContextExporter/D365ContextExporter/UI/SpecPickerControl.Designer.cs
@@ -24,56 +24,67 @@
             this.toolTip = new System.Windows.Forms.ToolTip(this.components);
             this.tableLayout.SuspendLayout();
             this.SuspendLayout();
-
+            // 
+            // lblSpec
+            // 
             this.lblSpec.AutoSize = true;
             this.lblSpec.Dock = System.Windows.Forms.DockStyle.Top;
             this.lblSpec.Location = new System.Drawing.Point(0, 0);
             this.lblSpec.Name = "lblSpec";
-            this.lblSpec.Size = new System.Drawing.Size(400, 17);
+            this.lblSpec.Size = new System.Drawing.Size(35, 13);
             this.lblSpec.TabIndex = 0;
             this.lblSpec.Text = "Spec:";
-
+            // 
+            // tableLayout
+            // 
             this.tableLayout.ColumnCount = 2;
             this.tableLayout.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
-            this.tableLayout.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 32F));
+            this.tableLayout.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 27F));
             this.tableLayout.Controls.Add(this.cmbSpecs, 0, 0);
             this.tableLayout.Controls.Add(this.btnRefresh, 1, 0);
             this.tableLayout.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.tableLayout.Location = new System.Drawing.Point(0, 17);
+            this.tableLayout.Location = new System.Drawing.Point(0, 13);
             this.tableLayout.Name = "tableLayout";
             this.tableLayout.RowCount = 1;
             this.tableLayout.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
-            this.tableLayout.Size = new System.Drawing.Size(400, 30);
+            this.tableLayout.Size = new System.Drawing.Size(343, 30);
             this.tableLayout.TabIndex = 1;
-
+            // 
+            // cmbSpecs
+            // 
             this.cmbSpecs.Dock = System.Windows.Forms.DockStyle.Fill;
             this.cmbSpecs.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.cmbSpecs.FormattingEnabled = true;
             this.cmbSpecs.Location = new System.Drawing.Point(3, 3);
             this.cmbSpecs.Name = "cmbSpecs";
-            this.cmbSpecs.Size = new System.Drawing.Size(362, 23);
+            this.cmbSpecs.Size = new System.Drawing.Size(310, 21);
             this.cmbSpecs.TabIndex = 0;
             this.cmbSpecs.SelectedIndexChanged += new System.EventHandler(this.cmbSpecs_SelectedIndexChanged);
-
+            // 
+            // btnRefresh
+            // 
             this.btnRefresh.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.btnRefresh.Location = new System.Drawing.Point(371, 3);
+            this.btnRefresh.Location = new System.Drawing.Point(319, 3);
             this.btnRefresh.Name = "btnRefresh";
-            this.btnRefresh.Size = new System.Drawing.Size(26, 24);
+            this.btnRefresh.Size = new System.Drawing.Size(21, 24);
             this.btnRefresh.TabIndex = 1;
             this.btnRefresh.Text = "↺";
+            this.toolTip.SetToolTip(this.btnRefresh, "Refresh spec list");
             this.btnRefresh.UseVisualStyleBackColor = true;
             this.btnRefresh.Click += new System.EventHandler(this.btnRefresh_Click);
-            this.toolTip.SetToolTip(this.btnRefresh, "Refresh spec list");
-
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            // 
+            // SpecPickerControl
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.Controls.Add(this.tableLayout);
             this.Controls.Add(this.lblSpec);
             this.Name = "SpecPickerControl";
-            this.Size = new System.Drawing.Size(400, 50);
+            this.Size = new System.Drawing.Size(343, 43);
             this.tableLayout.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
+
         }
 
         private System.Windows.Forms.Label lblSpec;
